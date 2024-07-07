6d7ab70490ea822629c372ffcbaddca4e8cbd242
programmer-alt <modstils88@mail.ru>
2024-07-07 20:08:37 +0400

    should handle API response

--- src/DataComponent.test.tsx
+++ src/DataComponent.test.tsx
@@ -1,8 +1,9 @@
 import {jest} from '@jest/globals';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor} from '@testing-library/react';
 import axios from 'axios';
 import DataComponent from './DataComponent';
 import { act } from 'react';
+import axiosMockAdapter from 'axios-mock-adapter';
 
 jest.mock('axios');
 
@@ -13,7 +14,7 @@ describe('DataComponent', () => {
     mockedGet.mockRejectedValue(new Error('Network Error'));
 
     // Заменяем console.log фиктивной функцией
-    const consoleSpy =  jest.fn();
+    const consoleSpy = jest.fn();
     console.log = consoleSpy; // Непосредственно заменяем глобальную функцию
 
     await act(async () => {
@@ -24,4 +25,30 @@ describe('DataComponent', () => {
     // Проверяем вызовы фиктивной функции console.log
     expect(consoleSpy).toHaveBeenCalledWith('Ошибка запроса на сервер', expect.any(Error));
   });
-});
+  it ('should handle API response', async () => {
+    const mockWeatherData = {
+      data: {
+        main: {
+          temp: 20,
+        },
+        wind: {
+          speed: 5,
+        },
+      },
+    };
+  
+    jest.spyOn(axios, 'get').mockResolvedValue(mockWeatherData);
+  
+    render(<DataComponent />);
+  
+    await waitFor(() => {
+      expect(screen.getByText(/Текущий график/i)).toBeInTheDocument();
+      expect(screen.getByText(/Измерения в Bar/i)).toBeInTheDocument();
+      // Проверяем, что данные о температуре и скорости ветра отображаются в компоненте
+      // Это предполагает, что компонент Graph корректно обрабатывает и отображает полученные данные
+    });
+  
+    jest.restoreAllMocks();
+  })
+
+})
